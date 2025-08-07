f4ef38ef4c9d4426da4a342868d282462aea594e
seshu <you@example.com>
2025-08-07 23:11:11 +0530

    grf

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, NgZone, signal } from '@angular/core';
 import { RouterOutlet,RouterModule } from '@angular/router';
-import { NgIf } from '@angular/common'; 
+import { NgIf ,NgFor} from '@angular/common'; 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterModule,NgIf],
+  imports: [RouterOutlet,RouterModule,NgIf,NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ShalomRestaurant');
   showContact = false;
+  images = ['image1.jpg','zoo.jpg','3.jpg','6.jpg'];
+   currentIndex = 0;
+
+  private zone = inject(NgZone);
+  private cdr = inject(ChangeDetectorRef);
+
+  ngOnInit() {
+    this.zone.runOutsideAngular(() => {
+      setInterval(() => {
+        this.zone.run(() => {
+          this.currentIndex = (this.currentIndex + 1) % this.images.length;
+          this.cdr.detectChanges(); // Force update
+        });
+      }, 3000);
+    });
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+    this.cdr.detectChanges(); // Make sure to update manually
+  }
 }
